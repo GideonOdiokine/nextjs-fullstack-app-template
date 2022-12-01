20528879433d0411f6552b06dffcfcbf9055b3f5
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2022-12-01 15:00:31 +0100

    feat: implement search component

--- components/layouts/primary/PrimaryLayout.mocks.ts
+++ components/layouts/primary/PrimaryLayout.mocks.ts
@@ -2,6 +2,7 @@ import { IPrimaryLayout } from './PrimaryLayout';
 
 const base: IPrimaryLayout = {
   children: '{{component}}',
+  title: 'About Page',
 };
 
 export const mockPrimaryLayoutProps = {
